ebf342a65305493af8a209460d7f67c259e923f6
michellebritoo <dev.michellebrito@gmail.com>
2024-12-30 21:06:34 -0300

    refactor: add user mapper

--- app/src/main/java/com/list/desafio/android/presentation/UsersViewModel.kt
+++ app/src/main/java/com/list/desafio/android/presentation/UsersViewModel.kt
@@ -2,10 +2,11 @@ package com.list.desafio.android.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.list.desafio.android.common.ext.entityToUIModel
+import com.list.desafio.android.common.ext.responseToEntity
+import com.list.desafio.android.common.ext.responseToUIModel
 import com.list.desafio.android.data.UsersRepository
-import com.list.desafio.android.data.local.entity.UserEntity
 import com.list.desafio.android.data.remote.UserResponse
-import com.list.desafio.android.presentation.adapter.UserUIModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -27,7 +28,9 @@ class UsersViewModel(private val repository: UsersRepository) : ViewModel() {
                 .onCompletion { sendUIEvent(UsersUIEvent.Loader(shouldShowLoad = false)) }
                 .catch { getLocalData() }
                 .collect { response ->
-                    sendUIEvent(UsersUIEvent.ShowUsersList(response.responseToUIModel()))
+                    sendUIEvent(
+                        UsersUIEvent.ShowUsersList(response.responseToUIModel())
+                    )
                     saveLocalData(response)
                 }
         }
@@ -55,36 +58,5 @@ class UsersViewModel(private val repository: UsersRepository) : ViewModel() {
 
     private fun sendUIEvent(event: UsersUIEvent) = viewModelScope.launch(Dispatchers.Main) {
         _viewState.emit(event)
-    }
-
-    private fun List<UserResponse>.responseToUIModel(): List<UserUIModel> {
-        return map { user ->
-            UserUIModel(
-                name = user.name.orEmpty(),
-                username = user.username.orEmpty(),
-                image = user.image.orEmpty()
-            )
-        }
-    }
-
-    private fun List<UserEntity>.entityToUIModel(): List<UserUIModel> {
-        return map { user ->
-            UserUIModel(
-                name = user.name.orEmpty(),
-                username = user.username.orEmpty(),
-                image = user.image.orEmpty()
-            )
-        }
-    }
-
-    private fun List<UserResponse>.responseToEntity(): List<UserEntity> {
-        return map { user ->
-            UserEntity(
-                id = user.id ?: 0,
-                name = user.name.orEmpty(),
-                username = user.username.orEmpty(),
-                image = user.image.orEmpty()
-            )
-        }
     }
 }
